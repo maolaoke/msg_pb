3b1ad5a5834f1de7ff9b24d60ab237ae9c80c08d
maolaoke <1249459910@qq.com>
2022-03-22 23:15:14 +0800

    5

--- msg.go
+++ msg.go
@@ -1,5 +1,7 @@
 package msg_pb
 
+import "time"
+
 const (
 	MsgOnline = 1
 	MsgOffline = 2
@@ -7,6 +9,8 @@ const (
 	MsgTasKMonitor = 4
 	MsgHasStockNotice = 5
 	MsgStopWork = 6
+	MsgGetClients = 7
+	MsgSuccessOrder = 8
 
 	AdminClientName = "admin"
 )
@@ -14,11 +18,13 @@ const (
 // 消息
 type Message struct {
 	MessageID 	  int `json:"message_id"`
-	ClientName    string `json:"client_name"`
+	ClientName    string `json:"client_name"` // MsgOnline使用
 	Users         []User `json:"users"`
 	SecKill       SecKillConf   `json:"sec_kill"`
 	Monitor       MonitorConf   `json:"monitor"`
-	HasStockSkuID string         `json:"has_stock_sku_id"`
+	HasStockSkuID string         `json:"has_stock_sku_id"`  // MsgHasStockNotice
+	Clients       []string 	`json:"clients"`
+	SuccessOrders  []SuccessOrderInfo
 }
 
 type SecKillConf struct {
@@ -41,4 +47,12 @@ type MonitorConf struct {
 type User struct {
 	NickName string `json:"nick_name"`
 	Cookie string `json:"cookie"`
+}
+
+type SuccessOrderInfo struct {
+	UserName string `json:"user_name"`
+	SubmitTime time.Time `json:"submit_time"`
+	DiffTime int `json:"diff_time"`
+	OrderID string `json:"order_id"`
+	Price string `json:"price"`
 }
